8335c4858023845cfcdbfa9cf9c80efe8873fb15
Rakibul Hasan <rakibul15-11826@diu.edu.bd>
2022-10-10 11:29:18 +0600

    first commit

---
+++ src/test/java/jenkins/Jenkins.java
@@ -0,0 +1,40 @@
+package jenkins;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Jenkins {
+	WebDriver driver;
+
+	@Test(priority = 1)
+	public void test() {
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("jenkins");
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]")).click();
+
+		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/a/h3")).click();
+
+	}
+
+	@Test(priority = 2)
+	public void quit() {
+
+		driver.quit();
+
+	}
+
+}
